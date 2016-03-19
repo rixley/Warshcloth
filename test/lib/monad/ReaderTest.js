5e3be1a58e217d91ea3739640de94df18667af2e
Ray Mendez <double0siete@gmail.com>
2016-03-19 09:31:01 -0400

    Add Reader flatMap

--- test/lib/monad/ReaderTest.js
+++ test/lib/monad/ReaderTest.js
@@ -12,4 +12,18 @@ describe('Reader monad', () => {
 
         assert.equal(result, 6);
     });
+
+    it('should implement flatMap', () => {
+        function push1() { return new Reader((arr) => arr.push(1)); }
+        function push2() { return new Reader((arr) => arr.push(2)); }
+        function pop() {
+            return new Reader((arr) => {
+                arr.pop();
+                return arr;
+            });
+        }
+
+        let result = push1().flatMap(push2).flatMap(push1).flatMap(pop).run([]);
+        assert.deepEqual(result, [1, 2]);
+    });
 });
